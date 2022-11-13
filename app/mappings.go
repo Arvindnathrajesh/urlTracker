a324e6730a262047fdab564763fda7aca5b6d231
Arvind Nath Rajesh <arvindnathrajesh@OX-0003.local>
2022-11-14 02:56:40 +0530

    added comments

--- app/mappings.go
+++ app/mappings.go
@@ -8,10 +8,10 @@ import (
 
 func MapUrls() {
 	router.GET("/ping", ping.Ping)
-	router.GET("/user/find", users.FindUser)
+	router.GET("/user/find", users.FindUser) // just CRUD operations
 	router.GET("/user/update", users.UpdateUser)
 	router.POST("/user/create", users.CreateUser)
-	router.POST("/linkData/create", link.CreateLinkData)
-	router.POST("/cron/click", link.UrlClicked)
-	router.GET("link/redirect", link.Redirect)
+	router.POST("/linkData/create", link.CreateLinkData) // API to create short URL
+	router.POST("/cron/click", link.UrlClicked)          // API to update and store click time on DB
+	router.GET("link/redirect", link.Redirect)           // API to redirect to a particular URL
 }
